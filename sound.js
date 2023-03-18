75dc2838dc4d76d2173772b0da5371e841b6fba5
matthiasmeissen <meissenmatthias@gmail.com>
2023-03-18 17:56:58 +0100

    Add additional oscialltor to sound more complex

--- sound.js
+++ sound.js
@@ -1,30 +1,36 @@
 class Sound {
     constructor() {
         this.audioContext = new AudioContext();
-        this.createOscillator();
+        this.createOscillators();
         this.isPlaying = false;
     }
 
-    createOscillator() {
-        this.oscillator = this.audioContext.createOscillator();
-        this.oscillator.type = 'sine';
-        this.oscillator.frequency.value = 440;
+    createOscillators() {
+        this.sineOscillator = this.audioContext.createOscillator();
+        this.sineOscillator.type = 'sine';
+        this.sineOscillator.frequency.value = 440;
+
+        this.sawOscillator = this.audioContext.createOscillator();
+        this.sawOscillator.type = 'sawtooth';
+        this.sawOscillator.frequency.value = 440;
 
         this.gain = this.audioContext.createGain();
-        this.oscillator.connect(this.gain);
+        this.sineOscillator.connect(this.gain);
+        this.sawOscillator.connect(this.gain);
         this.gain.connect(this.audioContext.destination);
     }
 
     playNote(frequency, duration) {
         if (!this.isPlaying) {
-            this.oscillator.start();
+            this.sineOscillator.start();
+            this.sawOscillator.start();
             this.isPlaying = true;
         }
 
-        this.oscillator.frequency.value = frequency;
+        this.sineOscillator.frequency.value = frequency;
+        this.sawOscillator.frequency.value = frequency;
 
-        this.gain.gain.setValueAtTime(0, this.audioContext.currentTime);
-        this.gain.gain.linearRampToValueAtTime(0.1, this.audioContext.currentTime + 0.1);
+        this.gain.gain.setValueAtTime(0.2, this.audioContext.currentTime);
         this.gain.gain.linearRampToValueAtTime(0, this.audioContext.currentTime + duration);
     }
 }
